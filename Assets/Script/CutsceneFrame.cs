159d0497f4ff926fe648edaf49eff0bef726a168
nthinhh39 <noobgame265@gmail.com>
2026-05-25 21:42:25 +0700

    cutscene

---
+++ Assets/Script/CutsceneFrame.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Serializable]
+public class CutsceneFrame
+{
+    public Sprite image;
+
+    [TextArea(3,10)]
+    public string dialogue;
+
+    [Header("Thời gian giữ sau khi chữ chạy xong")]
+    public float waitTime = 3f;
+}
