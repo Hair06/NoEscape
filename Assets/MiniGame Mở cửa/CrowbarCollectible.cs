4d378eec48fe2f6a8f8581ad5cd3825e150d4ab6
HolyTaii <taittps38535@gmail.com>
2026-07-10 04:15:12 +0700

    .

---
+++ Assets/MiniGame Mở cửa/CrowbarCollectible.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class CrowbarCollectible : MonoBehaviour
+{
+    [Header("Tên item")]
+    public string itemName = "Crowbar";
+
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI promptText;
+    [SerializeField] private string interactMessage = "Nhấn [E] để nhặt xà beng";
+
+    [Header("Âm thanh")]
+    [SerializeField] private AudioClip collectSound;
+
+    private bool isPlayerInside = false;
+
+    private void Start()
+    {
+        if (promptText != null) promptText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isPlayerInside && GameInputBridge.GetKeyDown(KeyCode.E))
+            Collect();
+    }
+
+    private void Collect()
+    {
+        PlayerInventory.Add(itemName);
+        Debug.Log("Đã nhặt Crowbar!");
+
+        if (collectSound != null)
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+
+        if (promptText != null) promptText.gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        isPlayerInside = true;
+        if (promptText != null)
+        {
+            promptText.text = interactMessage;
+            promptText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        isPlayerInside = false;
+        if (promptText != null) promptText.gameObject.SetActive(false);
+    }
+}
